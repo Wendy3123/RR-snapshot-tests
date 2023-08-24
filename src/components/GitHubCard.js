2ff06787f1ef7282d808c5cfb98119c8d63ef071
Wendy Wang <wendywangg928@gmail.com>
2023-08-23 21:48:40 -0400

    updated

---
+++ src/components/GitHubCard.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function GitHubCard() {
+  return (
+    <>
+      <h1>Github User: Wendy0928</h1>
+      <img
+        src="https://img.freepik.com/free-vector/girl-shy-character_1450-155.jpg?w=2000"
+        width={300}
+        alt="pic of me"
+      ></img>
+      <p>
+        I'm currently a full time student learning software development. I train
+        2-3 days a week for figure skating during my free time. I competed in 4
+        figure skating competitions so far and many more to come.{" "}
+      </p>
+    </>
+  );
+}
+
+export default GitHubCard;
